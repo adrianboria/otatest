016e7bb2200c6fa3f8986e3620e8a5c3e2cdc56b
adrianboria <66091450+adrianboria@users.noreply.github.com>
2025-06-02 14:48:41 -0300

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,27 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/adrianboria/otatest/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "boot.py")
+
+ota_updater.download_and_install_update_if_available()
+
+
+import machine
+import time
+
+led = machine.Pin(2,machine.Pin.OUT)
+
+while True:
+    led.on()
+    time.sleep(3)
+    led.off()
+    time.sleep(3)
+
