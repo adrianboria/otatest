64891b53534f1efdea53dd8cc90d897ffa54f30a
adrianboria <66091450+adrianboria@users.noreply.github.com>
2025-06-03 18:35:57 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -8,11 +8,10 @@ from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
 firmware_url = "https://raw.githubusercontent.com/adrianboria/otatest/"
-
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "boot.py")
-
 ota_updater.download_and_install_update_if_available()
 
+#------------------
 
 import machine
 import time
